0a445ccf1f88b8b8419bb5c46319aedd4f5ff4a9
loiane <loianeg@gmail.com>
2014-07-09 14:53:44 -0300

    added chapter 06

---
+++ chapter06/02-UsingSets.js
@@ -0,0 +1,17 @@
+var set = new Set();
+
+set.add(1);
+console.log(set.values()); //outputs ["1"]
+console.log(set.has(1));   //outputs true
+console.log(set.size());   //outputs 1
+
+set.add(2);
+console.log(set.values()); //outputs ["1", "2"]
+console.log(set.has(2));   //true
+console.log(set.size());   //2
+
+set.remove(1);
+console.log(set.values()); //outputs ["2"]
+
+set.remove(2);
+console.log(set.values()); //outputs []

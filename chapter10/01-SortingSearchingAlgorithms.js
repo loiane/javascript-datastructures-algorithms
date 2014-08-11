e9cd528bdc3d4f5519c2da4306e2613dcacdcbeb
loiane <loianeg@gmail.com>
2014-08-11 10:04:59 -0300

    added chapter 10

--- chapter10/01-SortingSearchingAlgorithms.js
+++ chapter10/01-SortingSearchingAlgorithms.js
@@ -24,6 +24,7 @@ function ArrayList(){
         var length = array.length;
 
         for (var i=0; i<length; i++){
+            console.log('--- ');
             for (var j=0; j<length-1; j++ ){
                 console.log('compare ' + array[j] + ' with ' + array[j+1]);
                 if (array[j] > array[j+1]){
@@ -38,6 +39,7 @@ function ArrayList(){
         var length = array.length;
 
         for (var i=0; i<length; i++){
+            console.log('--- ');
             for (var j=0; j<length-1-i; j++ ){
                 console.log('compare ' + array[j] + ' with ' + array[j+1]);
                 if (array[j] > array[j+1]){
@@ -191,4 +193,63 @@ function ArrayList(){
         }
         return array;
     };
+
+    this.sequentialSearch = function(item){
+
+        for (var i=0; i<array.length; i++){
+            if (item === array[i]){
+                return i;
+            }
+        }
+
+        return -1;
+    };
+
+    this.findMaxValue = function(){
+        var max = array[0];
+        for (var i=1; i<array.length; i++){
+            if (max < array[i]){
+                max = array[i];
+            }
+        }
+
+        return max;
+    };
+
+    this.findMinValue = function(){
+        var min = array[0];
+        for (var i=1; i<array.length; i++){
+            if (min > array[i]){
+                min = array[i];
+            }
+        }
+
+        return min;
+    };
+
+    this.binarySearch = function(item){
+        this.quickSort();
+
+        var low = 0,
+            high = array.length - 1,
+            mid, element;
+
+        while (low <= high){
+            mid = Math.floor((low + high) / 2);
+            element = array[mid];
+            console.log('mid element is ' + element);
+            if (element < item) {
+                low = mid + 1;
+                console.log('low is ' + low);
+            } else if (element > item) {
+                high = mid - 1;
+                console.log('high is ' + high);
+            } else {
+                console.log('found it');
+                return mid;
+            }
+        }
+        return -1;
+    };
+
 }

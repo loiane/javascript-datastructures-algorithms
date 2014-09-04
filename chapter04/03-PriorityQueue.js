a4526c06c9a65779e50b6dd8870a368d4cf61baa
loiane <loianeg@gmail.com>
2014-09-04 17:51:03 -0300

    code refactoring and bug fix

--- chapter04/03-PriorityQueue.js
+++ chapter04/03-PriorityQueue.js
@@ -1,46 +1,51 @@
 function PriorityQueue() {
 
-    this.items = [];
+    var items = [];
 
     function QueueElement (element, priority){
-        this.element = element;
-        this.priority = priority;
+        element = element;
+        priority = priority;
     }
 
     this.enqueue = function(element, priority){
         var queueElement = new QueueElement(element, priority);
 
-        if (this.isEmpty()){
-            this.items.push(queueElement);
+        if (isEmpty()){
+            items.push(queueElement);
         } else {
-            for (var i=0; i<this.items.length; i++){
-                if (queueElement.priority < this.items[i].priority){
-                    this.items.splice(i,0,queueElement);
+            var added = false;
+            for (var i=0; i<items.length; i++){
+                if (queueElement.priority < items[i].priority){
+                    items.splice(i,0,queueElement);
+                    added = true;
                     break;
                 }
+            }
+            if (!added){
+                items.push(queueElement);
             }
         }
     };
 
     this.dequeue = function(){
-        return this.items.shift();
+        return items.shift();
     };
 
     this.front = function(){
-        return this.items[0];
+        return items[0];
     };
 
     this.isEmpty = function(){
-        return this.items.length == 0;
+        return items.length == 0;
     };
 
     this.size = function(){
-        return this.items.length;
+        return items.length;
     };
 
-    this.print = function(){
-        for (var i=0; i<this.items.length; i++){
-            console.log(this.items[i].element + ' - ' + this.items[i].priority);
+    this. print = function(){
+        for (var i=0; i<items.length; i++){
+            console.log(items[i].element + ' - ' + items[i].priority);
         }
     };
 }
@@ -49,5 +54,7 @@ var priorityQueue = new PriorityQueue();
 priorityQueue.enqueue("John", 2);
 priorityQueue.enqueue("Jack", 1);
 priorityQueue.enqueue("Camila", 1);
+priorityQueue.enqueue("Maxwell", 2);
+priorityQueue.enqueue("Ana", 3);
 priorityQueue.print();
 

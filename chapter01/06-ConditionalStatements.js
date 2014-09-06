f4e3adcc454361b0ac583b506e05eabe32bf57d4
loiane <loianeg@gmail.com>
2014-09-06 14:48:55 -0300

    renamed files

--- chapter01/06-ConditionalStatements.js
+++ chapter01/06-ConditionalStatements.js
@@ -1,12 +1,12 @@
 /* Example 01 - if */
 var num = 1;
-if (num == 1) {
+if (num === 1) {
     console.log("num is equal to 1");
 }
 
 /* Example 02 - if-else */
 var num = 0;
-if (num == 1) {
+if (num === 1) {
     console.log("num is equal to 1");
 } else {
     console.log("num is not equal to 1, the value of num is " + num);
@@ -14,11 +14,11 @@ if (num == 1) {
 
 /* Example 03 - if-else-if-else... */
 var month = 5;
-if (month == 1) {
+if (month === 1) {
     console.log("January");
-} else if (month == 2){
+} else if (month === 2){
     console.log("February");
-} else if (month == 3){
+} else if (month === 3){
     console.log("March");
 } else {
     console.log("Month is not January, February or March");
@@ -39,3 +39,13 @@ switch(month) {
     default:
         console.log("Month is not January, February or March");
 }
+
+/* Example 05 - ternary operator - if..else */
+if (num === 1){
+    num--;
+} else {
+    num++;
+}
+
+//is the same as
+(num === 1) ? num-- : num++;

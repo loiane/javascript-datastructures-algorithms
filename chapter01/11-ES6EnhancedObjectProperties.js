59055a9c0a3f781a4c9b27f8fe0f64b0a360efbb
Loiane Groner <loianeg@gmail.com>
2016-01-09 14:06:58 -0200

    ES6: enhanceded object properties

---
+++ chapter01/11-ES6EnhancedObjectProperties.js
@@ -0,0 +1,30 @@
+// Destructuring Assignment + Property Shorthand
+var [x, y] = ['a', 'b'];
+var obj = { x, y };
+console.log(obj); // { x: "a", y: "b" }
+
+//code above is the same as
+var x = 'a';
+var y = 'b';
+
+var obj2 = { x: x, y: y };
+console.log(obj2); // { x: "a", y: "b" }
+
+
+// Method Properties
+var hello = {
+    name : 'abcdef',
+    printHello(){
+        console.log('Hello');
+    }
+}
+console.log(hello.printHello());
+
+//code above is the same as:
+var hello2 = {
+    name: 'abcdef',
+    printHello: function printHello() {
+        console.log('Hello');
+    }
+};
+console.log(hello2.printHello());

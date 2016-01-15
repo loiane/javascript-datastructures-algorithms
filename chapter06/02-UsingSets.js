275ed76c6fbbb0c145b72792768967d5c790810c
Ward Ruth <ward.ruth@gmail.com>
2016-01-15 12:02:17 -0800

    update Set values() and valuesLegacy() to return items values instead of items keys

--- chapter06/02-UsingSets.js
+++ chapter06/02-UsingSets.js
@@ -1,18 +1,18 @@
 var set = new Set();
 
 set.add(1);
-console.log(set.values()); //outputs ["1"]
+console.log(set.values()); //outputs [1]
 console.log(set.has(1));   //outputs true
 console.log(set.size());   //outputs 1
 
 set.add(2);
-console.log(set.values()); //outputs ["1", "2"]
+console.log(set.values()); //outputs [1, 2]
 console.log(set.has(2));   //true
 console.log(set.size());   //2
 console.log(set.sizeLegacy());   //3
 
 set.remove(1);
-console.log(set.values()); //outputs ["2"]
+console.log(set.values()); //outputs [2]
 
 set.remove(2);
 console.log(set.values()); //outputs []

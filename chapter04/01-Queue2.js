cc3cdae6ec7f600c9e3b4a2af541e015f6f70242
Loiane Groner <loianeg@gmail.com>
2016-01-23 19:18:22 -0200

    Queue class ES6 syntax

---
+++ chapter04/01-Queue2.js
@@ -0,0 +1,51 @@
+let Queue2 = (function () {
+
+    const items = new WeakMap();
+
+    class Queue2 {
+
+        constructor () {
+            items.set(this, []);
+        }
+
+        enqueue(element) {
+            let q = items.get(this);
+            q.push(element);
+            items.set(this, q)
+        }
+
+        dequeue() {
+            let q = items.get(this);
+            let r = q.shift();
+            items.set(this, q);
+            return r;
+        }
+
+        front() {
+            let q = items.get(this);
+            return q[0];
+        }
+
+        isEmpty(){
+            return items.get(this).length == 0;
+        }
+
+        size(){
+            let q = items.get(this);
+            return q.length;
+        }
+
+        clear(){
+            items.set(this, []);
+        }
+
+        print(){
+            console.log(this.toString());
+        }
+
+        toString(){
+            return items.get(this).toString();
+        }
+    }
+    return Queue2;
+})();

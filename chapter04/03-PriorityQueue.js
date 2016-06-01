5d9f8c68dca6c43f4c6a25fc769c7e6af15c4511
Loiane Groner <loiane@Loianes-MacBook-Pro.local>
2016-06-01 00:02:58 -0300

    reviewed code bundle

--- chapter04/03-PriorityQueue.js
+++ chapter04/03-PriorityQueue.js
@@ -50,7 +50,5 @@ let priorityQueue = new PriorityQueue();
 priorityQueue.enqueue("John", 2);
 priorityQueue.enqueue("Jack", 1);
 priorityQueue.enqueue("Camila", 1);
-priorityQueue.enqueue("Maxwell", 2);
-priorityQueue.enqueue("Ana", 3);
 priorityQueue.print();
 

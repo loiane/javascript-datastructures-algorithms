b2f6e3af1b2ec8ab457aa554d091da66f77ad0be
loiane <loianeg@gmail.com>
2017-09-17 16:44:35 -0300

    changed var name

--- examples/chapter01/02-Variables.js
+++ examples/chapter01/02-Variables.js
@@ -1,16 +1,17 @@
 // @ts-check
+/* eslint-disable */
 
 var num = 1; // {1}
 num = 3; // {2}
 
 var price = 1.5; // {3}
-var name = 'Packt'; // {4}
+var myName = 'Packt'; // {4}
 var trueValue = true; // {5}
 var nullVar = null; // {6}
 var und; // {7}
 
 console.log('num: ' + num);
-console.log('name: ' + name);
+console.log('myName: ' + myName);
 console.log('trueValue: ' + trueValue);
 console.log('price: ' + price);
 console.log('nullVar: ' + nullVar);

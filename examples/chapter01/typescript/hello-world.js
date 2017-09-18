e50ce8a73592ac9c877bf99770b90bbd5aaf443d
loiane <loianeg@gmail.com>
2017-09-17 23:50:19 -0300

    chapter 01: intro TS

---
+++ examples/chapter01/typescript/hello-world.js
@@ -0,0 +1,8 @@
+var myName = 'Packt';
+// myName = 10;
+var age = 20; // number
+var existsFlag = true; // boolean
+var language = 'JavaScript'; // string
+var favoriteLanguage;
+var langs = ['JavaScript', 'Ruby', 'Python'];
+favoriteLanguage = langs[0];
---
+++ examples/chapter01/typescript/hello-world.ts
@@ -0,0 +1,10 @@
+let myName = 'Packt';
+// myName = 10;
+
+let age = 20; // number
+let existsFlag = true; // boolean
+let language = 'JavaScript'; // string
+
+let favoriteLanguage: string;
+let langs = ['JavaScript', 'Ruby', 'Python'];
+favoriteLanguage = langs[0];

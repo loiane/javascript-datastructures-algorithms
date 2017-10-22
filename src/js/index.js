4201d4b016612b614282ca48ef57e045f52a19dc
loiane <loianeg@gmail.com>
2017-10-22 16:09:35 -0200

    chapter 06: [Sets]

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,24 @@
-import { hotPotato } from './others/hot-potato';
-import { palindromeChecker } from './others/palindrome-checker';
-import Deque from './data-structures/deque';
-import Queue from './data-structures/queue';
-import { hanoi, hanoiStack } from './others/hanoi';
-import { baseConverter, decimalToBinary } from './others/base-converter';
-import StackArray from './data-structures/stack-array';
-import Stack from './data-structures/stack';
-import { parenthesesChecker } from './others/balanced-symbols';
-
 import * as _util from './util';
+
+export const util = _util;
+
+export { default as StackArray } from './data-structures/stack-array';
+export { default as Stack } from './data-structures/stack';
+export { default as hanoi } from './others/hanoi';
+export { default as hanoiStack } from './others/hanoi';
+export { default as baseConverter } from './others/base-converter';
+export { default as decimalToBinary } from './others/base-converter';
+export { default as parenthesesChecker } from './others/balanced-symbols';
+
+export { default as Queue } from './data-structures/queue';
+export { default as Deque } from './data-structures/deque';
+export { default as hotPotato } from './others/hot-potato';
+export { default as palindromeChecker } from './others/palindrome-checker';
 
 export { default as LinkedList } from './data-structures/linked-list';
 export { default as DoublyLinkedList } from './data-structures/doubly-linked-list';
 export { default as CircularLinkedList } from './data-structures/circular-linked-list';
 export { default as SortedLinkedList } from './data-structures/sorted-linked-list';
 export { default as StackLinkedList } from './data-structures/stack-linked-list';
+
 export { default as Set } from './data-structures/set';
-
-export const util = _util;
-
-export {
-  Stack,
-  StackArray,
-  parenthesesChecker,
-  baseConverter,
-  decimalToBinary,
-  hanoi,
-  hanoiStack,
-  Queue,
-  Deque,
-  hotPotato,
-  palindromeChecker
-};
